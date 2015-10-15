aac4ec08ecff0909284998f9c1173cc0d93a2c95
Fernando Daciuk <f.daciuk@gmail.com>
2015-10-15 18:06:12 -0300

    Add all files to watch

--- template/gulp-config/tasks/watch.js
+++ template/gulp-config/tasks/watch.js
@@ -1,9 +1,11 @@
 'use strict';
 
 let gulp = require('gulp');
-var paths = require('../paths');
+let paths = require('../paths');
+
+let allFiles = paths.js.concat(paths.testFiles);
 
 gulp.task('watch', () => {
-	gulp.watch(paths.js, [ 'lint' ]);
 	gulp.start('lint');
+	gulp.watch(allFiles, [ 'lint' ]);
 });
